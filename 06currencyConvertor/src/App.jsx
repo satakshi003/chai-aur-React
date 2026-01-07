73b11468def53558ce9447c19b4ea143b271e626
satakshi003 <soumya3955@gmail.com>
2026-01-07 22:32:02 +0530

    download all packages and import tailwind

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='text-3xl bg-orange-600'>Currency app with chai</h1>
+    </>
+  )
+}
+
+export default App
