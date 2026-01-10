0dfb8b92f192614892ebcb8574f43258f6dc2d71
satakshi003 <soumya3955@gmail.com>
2026-01-11 01:04:29 +0530

    Context API project

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,17 @@
+import Login from "./Components/Login"
+import Profile from "./Components/Profile"
+import UserContextProvider from "./context/UserContextProvider"
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+     <h1>React with Chai</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
