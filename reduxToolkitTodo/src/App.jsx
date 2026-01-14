ca0257761a8d2950a5cdd6679a045861c83fddeb
satakshi003 <soumya3955@gmail.com>
2026-01-15 00:20:05 +0530

    completing reduxToolkitTodo

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,3 +1,5 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
 
 
 function App() {
@@ -6,6 +8,8 @@ function App() {
   return (
     <>
       <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
